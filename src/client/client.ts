102d437cdd99fc04f5ef6fcfb1d3bdf893989c07
KEEN <kyh0927@live.co.kr>
2021-10-19 23:45:42 +0900

    feat : camera, scene, renderer

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,18 +1,26 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+// #1
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color("#000000");
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 2;
+// #2
+const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10);
+const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2);
+camera1.position.z = 2;
+camera2.position.z = 2;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// #3
+const canvas1 = document.getElementById("testCanvas1") as HTMLCanvasElement;
+const canvas2 = document.getElementById("testCanvas2") as HTMLCanvasElement;
+const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 });
+const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
+renderer1.setSize(200, 200);
+renderer2.setSize(200, 200);
+
+// 마우스 드래그로 막 움직임
+new OrbitControls(camera1, renderer1.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
@@ -25,9 +33,8 @@ scene.add(cube);
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera1.aspect = window.innerWidth / window.innerHeight;
+  camera1.updateProjectionMatrix();
   render();
 }
 
@@ -41,7 +48,8 @@ function animate() {
 }
 
 function render() {
-  renderer.render(scene, camera);
+  renderer1.render(scene, camera1);
+  renderer2.render(scene, camera2);
 }
 
 animate();
