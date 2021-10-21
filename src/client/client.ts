a4238108c53f3b1575ee609fab2c2ae3270b4591
KEEN <kyh0927@live.co.kr>
2021-10-21 22:53:42 +0900

    feat : 3d object

--- src/client/client.ts
+++ src/client/client.ts
@@ -5,21 +5,22 @@ import { GUI } from "three/examples/jsm/libs/dat.gui.module";
 
 // #1
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 // scene.background = new THREE.Color("#000000");
 
 // #2
 const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10);
-const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2);
+// const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2);
 camera1.position.z = 2;
-camera2.position.z = 2;
+// camera2.position.z = 2;
 
 // #3
 const canvas1 = document.getElementById("testCanvas1") as HTMLCanvasElement;
-const canvas2 = document.getElementById("testCanvas2") as HTMLCanvasElement;
+// const canvas2 = document.getElementById("testCanvas2") as HTMLCanvasElement;
 const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 });
-const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
+// const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
 renderer1.setSize(200, 200);
-renderer2.setSize(200, 200);
+// renderer2.setSize(200, 200);
 
 // 마우스 드래그로 막 움직임
 // 컨트롤스 상수에 집어넣고 change이벤트 리스너로 렌더링 시키면 OrbitControls이 가능하다
@@ -63,14 +64,26 @@ document.body.appendChild(stats.dom);
 // GUI 추가
 const gui = new GUI();
 const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeFolder.add(cube, "visible");
 cubeFolder.open();
 
-const cameraFolder = gui.addFolder("camera");
-cameraFolder.add(camera1.position, "z", 0, 10);
-cameraFolder.open();
+const cubeRotationFolder = cubeFolder.addFolder("Rotation");
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeRotationFolder.open();
+
+const cubePositionFolder = cubeFolder.addFolder("Position");
+cubePositionFolder.add(cube.position, "x", -10, 10, 0.1);
+cubePositionFolder.add(cube.position, "y", -10, 10, 0.1);
+cubePositionFolder.add(cube.position, "z", -10, 10, 0.1);
+cubePositionFolder.open();
+
+const cubeScaleFolder = cubeFolder.addFolder("Scale");
+cubeScaleFolder.add(cube.scale, "x", -5, 5);
+cubeScaleFolder.add(cube.scale, "y", -5, 5);
+cubeScaleFolder.add(cube.scale, "z", -5, 5);
+cubeScaleFolder.open();
 
 function render() {
   renderer1.render(scene, camera1);
