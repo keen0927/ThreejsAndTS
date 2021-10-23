fcabcf971073da5641bdf89b54dfe9160c566c54
KEEN <kyh0927@live.co.kr>
2021-10-23 23:05:25 +0900

    feat : Object 3d Hierarchy

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,92 +3,132 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
 import { GUI } from "three/examples/jsm/libs/dat.gui.module";
 
-// #1
 const scene = new THREE.Scene();
 scene.add(new THREE.AxesHelper(5));
-// scene.background = new THREE.Color("#000000");
+console.log(scene);
 
-// #2
-const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10);
-// const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2);
-camera1.position.z = 2;
-// camera2.position.z = 2;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.x = 4;
+camera.position.y = 4;
+camera.position.z = 4;
 
-// #3
-const canvas1 = document.getElementById("testCanvas1") as HTMLCanvasElement;
-// const canvas2 = document.getElementById("testCanvas2") as HTMLCanvasElement;
-const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 });
-// const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
-renderer1.setSize(200, 200);
-// renderer2.setSize(200, 200);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// 마우스 드래그로 막 움직임
-// 컨트롤스 상수에 집어넣고 change이벤트 리스너로 렌더링 시키면 OrbitControls이 가능하다
-const controls = new OrbitControls(camera1, renderer1.domElement);
-controls.addEventListener("change", render);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(8, 0, 0);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  wireframe: true,
-});
+const light1 = new THREE.PointLight();
+light1.position.set(10, 10, 10);
+scene.add(light1);
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const light2 = new THREE.PointLight();
+light2.position.set(-10, 10, 10);
+scene.add(light2);
+
+const object1 = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(),
+  new THREE.MeshPhongMaterial({ color: 0xff0000 })
+);
+object1.position.set(4, 0, 0);
+scene.add(object1);
+object1.add(new THREE.AxesHelper(5));
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(),
+  new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+);
+object2.position.set(4, 0, 0);
+object1.add(object2);
+object2.add(new THREE.AxesHelper(5));
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(),
+  new THREE.MeshPhongMaterial({ color: 0x0000ff })
+);
+object3.position.set(4, 0, 0);
+object2.add(object3);
+object3.add(new THREE.AxesHelper(5));
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-  camera1.aspect = window.innerWidth / window.innerHeight;
-  camera1.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
 
-function animate() {
-  requestAnimationFrame(animate);
+const gui = new GUI();
+const object1Folder = gui.addFolder("Object1");
+object1Folder.add(object1.position, "x", 0, 10, 0.01).name("X Position");
+object1Folder
+  .add(object1.rotation, "x", 0, Math.PI * 2, 0.01)
+  .name("X Rotation");
+object1Folder.add(object1.scale, "x", 0, 2, 0.01).name("X Scale");
+object1Folder.open();
+const object2Folder = gui.addFolder("Object2");
+object2Folder.add(object2.position, "x", 0, 10, 0.01).name("X Position");
+object2Folder
+  .add(object2.rotation, "x", 0, Math.PI * 2, 0.01)
+  .name("X Rotation");
+object2Folder.add(object2.scale, "x", 0, 2, 0.01).name("X Scale");
+object2Folder.open();
+const object3Folder = gui.addFolder("Object3");
+object3Folder.add(object3.position, "x", 0, 10, 0.01).name("X Position");
+object3Folder
+  .add(object3.rotation, "x", 0, Math.PI * 2, 0.01)
+  .name("X Rotation");
+object3Folder.add(object3.scale, "x", 0, 2, 0.01).name("X Scale");
+object3Folder.open();
 
-  stats.begin();
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
-  stats.end();
-
-  render();
-
-  // stats 업데이트
-  // stats.update();
-}
-
-// stats 추가
 const stats = Stats();
 document.body.appendChild(stats.dom);
 
-// GUI 추가
-const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-cubeFolder.add(cube, "visible");
-cubeFolder.open();
+const debug = document.getElementById("debug1") as HTMLDivElement;
 
-const cubeRotationFolder = cubeFolder.addFolder("Rotation");
-cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
-cubeRotationFolder.open();
-
-const cubePositionFolder = cubeFolder.addFolder("Position");
-cubePositionFolder.add(cube.position, "x", -10, 10, 0.1);
-cubePositionFolder.add(cube.position, "y", -10, 10, 0.1);
-cubePositionFolder.add(cube.position, "z", -10, 10, 0.1);
-cubePositionFolder.open();
-
-const cubeScaleFolder = cubeFolder.addFolder("Scale");
-cubeScaleFolder.add(cube.scale, "x", -5, 5);
-cubeScaleFolder.add(cube.scale, "y", -5, 5);
-cubeScaleFolder.add(cube.scale, "z", -5, 5);
-cubeScaleFolder.open();
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  render();
+  const object1WorldPosition = new THREE.Vector3();
+  object1.getWorldPosition(object1WorldPosition);
+  const object2WorldPosition = new THREE.Vector3();
+  object2.getWorldPosition(object2WorldPosition);
+  const object3WorldPosition = new THREE.Vector3();
+  object3.getWorldPosition(object3WorldPosition);
+  debug.innerText =
+    "Red\n" +
+    "Local Pos X : " +
+    object1.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object1WorldPosition.x.toFixed(2) +
+    "\n" +
+    "\nGreen\n" +
+    "Local Pos X : " +
+    object2.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object2WorldPosition.x.toFixed(2) +
+    "\n" +
+    "\nBlue\n" +
+    "Local Pos X : " +
+    object3.position.x.toFixed(2) +
+    "\n" +
+    "World Pos X : " +
+    object3WorldPosition.x.toFixed(2) +
+    "\n";
+  stats.update();
+}
 
 function render() {
-  renderer1.render(scene, camera1);
-  // renderer2.render(scene, camera2);
+  renderer.render(scene, camera);
 }
 
 animate();
-render();
